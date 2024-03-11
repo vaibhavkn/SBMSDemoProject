31765e58b57341c66b2dbabe60ecc116cedb215e
Vaibhav Bapurao Kalyankar <kvaibhav@cdac.in>
2024-03-11 11:52:50 +0530

    Started : New Project For SBMS and CICD

---
+++ src/main/java/com/cdac/hcdc/firstSpringBoot/FirstController.java
@@ -0,0 +1,13 @@
+package com.cdac.hcdc.firstSpringBoot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
